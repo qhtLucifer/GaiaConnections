0e61fcef7c414b11185617b5de5462a4f42534dd
qhtLucifer <qhtLucifer@foxmail.com>
2021-03-05 21:08:36 +0800

    Open the socket in GetSocket() to fix the bug of that socket is not open when used.

--- PacketConnection/Sender.cpp
+++ PacketConnection/Sender.cpp
@@ -5,10 +5,17 @@ namespace Gaia::Connections::PacketConnection
 	/// Acquire the socket for sending packet.
 	boost::asio::ip::udp::socket& Sender::GetSocket()
 	{
-		// Use inner static variable to prevent exception from escaping during its construction.
-		static boost::asio::io_context context_instance {};
-		static boost::asio::ip::udp::socket socket_instance {context_instance};
-		return socket_instance;
+        static std::unique_ptr<boost::asio::io_context> context_instance;
+        static std::unique_ptr<boost::asio::ip::udp::socket> socket_instance;
+
+        if (!socket_instance)
+        {
+            context_instance = std::make_unique<boost::asio::io_context>();
+            socket_instance = std::make_unique<boost::asio::ip::udp::socket>(*context_instance);
+            socket_instance->open(boost::asio::ip::udp::v4());
+        }
+
+        return *socket_instance;
 	}
 
     /// Static function for sending packet that has been constructed.
@@ -16,7 +23,6 @@ namespace Gaia::Connections::PacketConnection
     {
         // Send the data of packet to the packet's destination
         GetSocket().send_to(boost::asio::buffer(packet.Data), packet.Address);
-
     }
 
     /// Static function in order to sending packet only for temporariness without constructing Packet.
@@ -24,6 +30,5 @@ namespace Gaia::Connections::PacketConnection
     {
         // Send the data to the destination described in the parameter
         GetSocket().send_to(boost::asio::buffer(data), address);
-
     }
 }
